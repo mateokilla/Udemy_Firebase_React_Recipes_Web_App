d4a35dacbeb2cd4f86b298ca39acf8a0ac23fae8
mateokilla <nitro_5_bernath_mark@outlook.hu>
2023-02-15 14:08:50 +0100

    update again

--- src/FirebaseFirestoreService.js
+++ src/FirebaseFirestoreService.js
@@ -6,11 +6,60 @@ const createDocument = (collection, document) => {
   return firestore.collection(collection).add(document);
 };
 
-
-
-
-const FirebaseFireStoreService = {
-  createDocument,
+const readDocument = (collection, id) => {
+  return firestore.collection(collection).doc(id).get();
 };
 
-export default FirebaseFireStoreService;
+const readDocuments = async ({
+  collection,
+  queries,
+  orderByField,
+  orderByDirection,
+  perPage,
+  cursorId,
+}) => {
+  let collectionRef = firestore.collection(collection);
+
+  if (queries && queries.length > 0) {
+    for (const query of queries) {
+      collectionRef = collectionRef.where(
+        query.field,
+        query.condition,
+        query.value
+      );
+    }
+  }
+
+  if (orderByField && orderByDirection) {
+    collectionRef = collectionRef.orderBy(orderByField, orderByDirection);
+  }
+
+  if (perPage) {
+    collectionRef = collectionRef.limit(perPage);
+  }
+
+  if (cursorId) {
+    const document = await readDocument(collection, cursorId);
+
+    collectionRef = collectionRef.startAfter(document);
+  }
+
+  return collectionRef.get();
+};
+
+const updateDocument = (collection, id, document) => {
+  return firestore.collection(collection).doc(id).update(document);
+};
+
+const deleteDocument = (collection, id) => {
+  return firestore.collection(collection).doc(id).delete();
+};
+
+const FirebaseFirestoreService = {
+  createDocument,
+  readDocuments,
+  updateDocument,
+  deleteDocument,
+};
+
+export default FirebaseFirestoreService;
