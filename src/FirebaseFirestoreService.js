c7b82c6bf6d82e3ec8f98bba7a40b5542347edec
mateokilla <nitro_5_bernath_mark@outlook.hu>
2023-02-15 13:32:26 +0100

    update firestore

---
+++ src/FirebaseFirestoreService.js
@@ -0,0 +1,16 @@
+import firebase from "./FirebaseConfig";
+
+const firestore = firebase.firestore();
+
+const createDocument = (collection, document) => {
+  return firestore.collection(collection).add(document);
+};
+
+
+
+
+const FirebaseFireStoreService = {
+  createDocument,
+};
+
+export default FirebaseFireStoreService;
